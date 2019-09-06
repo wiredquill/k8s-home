64b8b653b2d84a987a11eca8f5037d11f7a1aec6
WiredQuill <wiredquill@gmail.com>
2019-09-06 16:24:50 -0700

    Initial Upload

---
+++ legacy/course_files/scripts/rmt-sync-enable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo rmt-cli product enable 1772
+sudo rmt-cli product enable 1790
+sudo rmt-cli product enable 1776
+sudo rmt-cli product enable 1808
+sudo rmt-cli product enable 1780
+sudo rmt-cli product enable 1863
+sudo rmt-cli product enable 1763
